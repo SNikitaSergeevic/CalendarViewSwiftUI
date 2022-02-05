f82e7c8d71c9120840c76d2ba9b28d654f749798
SNikitaSergeevic <snikitasergeevich@gmail.com>
2022-02-06 00:24:08 +0300

    Initial Commit

---
+++ CalendarSwiftUI/CalendarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSwiftUIApp.swift
+//  CalendarSwiftUI
+//
+//  Created by ProstoKakTo on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
